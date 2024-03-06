860b1428bfe53cf2c7f7d685cbeda8af7e757d7f
Asish Mahapatra <asishmahapatra918@gmail.com>
2024-03-06 11:00:01 +0530

    main: Initialised Backend Repository with Boilerplate

---
+++ src/main/java/com/kdu/ibebackend/IbeBackendApplication.java
@@ -0,0 +1,12 @@
+package com.kdu.ibebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbeBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kdu/ibebackend/IbeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdu.ibebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
