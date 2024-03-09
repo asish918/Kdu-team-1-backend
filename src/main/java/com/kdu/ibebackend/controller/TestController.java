8a25065cc092ea1c5091c098450206c8edf7e19d
Asish Mahapatra <asishmahapatra918@gmail.com>
2024-03-09 12:32:12 +0530

    IBT-22: Multi Environment setup done for backend

--- src/main/java/com/kdu/ibebackend/controller/TestController.java
+++ src/main/java/com/kdu/ibebackend/controller/TestController.java
@@ -1,12 +1,10 @@
 package com.kdu.ibebackend.controller;
 
 import com.kdu.ibebackend.entities.Room;
-import com.kdu.ibebackend.entities.Tenant;
 import com.kdu.ibebackend.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,15 +29,6 @@ public class TestController {
     public Room roomById(@Argument Long id) {
         Optional<Room> roomRes = roomRepository.findById(id);
 
-        if(roomRes.isPresent()) {
-            return roomRepository.findById(id).get();
-        }
-
-        return null;
+        return roomRes.orElse(null);
     }
-
-//    @SchemaMapping
-//    public Tenant tenant(Room room) {
-//        return Tenant.getById(room.tenantId());
-//    }
 }
